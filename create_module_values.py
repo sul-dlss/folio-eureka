7ee0b208e54e1f98493637b41e95ef450e8d2a68
Shelley Doljack <sdoljack@stanford.edu>
2026-01-29 12:41:30 -0700

    Adds flag to set production-level replicaset counts in module overrides.yaml.

--- create_module_values.py
+++ create_module_values.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('filename')
 parser.add_argument('-m', '--modules', help='a list of modules to install, each with -m flag', nargs="+", action='extend')
 parser.add_argument('-n', '--namespace', required=True, help='the Kubernetes namespace for the applications')
+parser.add_argument('-p', '--prod_replicasets', action="store_true", help="use production-level replicaset counts, else replicaCount=1")
 
 args = parser.parse_args()
 
@@ -29,7 +30,30 @@ S3_MODULES = [
     "mod-users",
 ]
 
-def base_override(name, version):
+ONE_REPLICASET = [
+    "mod-data-export",
+    "mod-data-export-spring",
+    "mod-data-export-worker",
+    "mod-data-import",
+    "mod-erm-usage-harvester",
+    "mod-quick-marc",
+]
+
+THREE_REPLICASET = [
+    "mod-circulation",
+    "mod-circulation-storage",
+    "mod-configuration",
+    "mod-finance",
+    "mod-finance-storage",
+    "mod-inventory",
+    "mod-inventory-storage",
+    "mod-orders",
+    "mod-orders-storage",
+    "mod-users",
+    "mod-users-bl",
+]
+
+def base_override(name, version, prod_rs):
     data = {
         "image": {"repository": f"folioorg/{name}", "tag": f"{version}"},
         "podSecurityContext": {"fsGroup": 2000},
@@ -64,7 +88,14 @@ def base_override(name, version):
         del data['integrations']['db']
         del data['integrations']['kafka']
         data['integrations']['eureka-edge'] = {"enabled": True, "existingSecret": "eureka-edge"}
-    
+
+    if prod_rs:
+        data["replicaCount"] = 2
+        if name in ONE_REPLICASET:
+            data["replicaCount"] = 1
+        if name in THREE_REPLICASET:
+            data["replicaCount"] = 3
+
     return yaml.dump(data)
 
 
@@ -75,13 +106,14 @@ modules = data['modules']
 
 if args.modules:
     modules = [obj for obj in data['modules'] if obj['name'] in args.modules]
-        
+
 for module in modules:
     name = module['name']
     version = module['version']
     dir = Path(f"{args.namespace}/modules/{name}")
     dir.mkdir(parents=True, exist_ok=True)
+    prod_rs = args.prod_replicasets
     
     filename = f"{dir}/overrides.yaml"
     with open(filename, 'w') as file:
-        file.write(base_override(name, version))
+        file.write(base_override(name, version, prod_rs))
