057ceecf89a8cb95a51f5d0cc8cbcc03fd614b11
Shelley Doljack <sdoljack@stanford.edu>
2026-02-11 16:22:05 -0700

    Removes shell scripts.

--- create_admin_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 bootstrap_admin_user.py --create_user --email $LIBSYS_ADMIN_EMAIL --first_name Libsys --last_name Admin --password $LIBSYS_PASSWORD --username $LIBSYS_USER
--- update_admin_role.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 bootstrap_admin_user.py --username $LIBSYS_USER
