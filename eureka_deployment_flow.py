e1811d31e43aac3fe339addcf1d19a17b40ed299
Shelley Doljack <sdoljack@stanford.edu>
2026-02-13 20:11:48 -0700

    Adds script to register apps, modules, create tenant, entitle apps.

---
+++ eureka_deployment_flow.py
@@ -0,0 +1,256 @@
+import argparse
+import httpx
+import json
+import os
+
+from pathlib import Path
+
+parser = argparse.ArgumentParser(
+                    prog="EurekaDeploymentFlow",
+                    description="Register applications, register modules, create tenant, create entitlements",
+                    epilog="-------")
+
+parser.add_argument("filename", nargs="*", help="The application descriptor JSON file or files to process. If not provided, all JSON files in the namespace directory will be processed.")
+parser.add_argument("-a", "--register_apps", action="store_true", default=False, help="register applications")
+parser.add_argument("-d", "--delete_apps", action="store_true", default=False, help="delete applications (for upgrading flower release)")
+parser.add_argument("-e", "--entitle", action="store_true", default=False, help="entitle applications")
+parser.add_argument("-m", "--register_modules", action="store_true", default=False, help="register modules for discovery")
+parser.add_argument("-u", "--reregister_modules", action="store_true", default=False, help="re-register modules for discovery")
+parser.add_argument("-n", "--namespace", required=True, help="the Kubernetes namespace for the applications.")
+parser.add_argument("-t", "--create_tenant", action="store_true", default=False, help="create tenant")
+
+args = parser.parse_args()
+
+def main(file):
+    global token
+    token = _token()
+    mgr_app_url = os.getenv("MGR_APP_URL", "http://mgr-applications")
+    mgr_tenants_url = os.getenv("MGR_TENANTS_URL", "http://mgr-tenants")
+    mgr_entitle_url = os.getenv("MGR_ENTITLE_URL", "http://mgr-tenant-entitlements")
+    with open(file, "r") as fo:
+        data = json.load(fo)
+        app_id = data["id"]
+        if args.register_apps:
+            print(f"Register {app_id}")
+            register_applications(token, data, mgr_app_url)
+            registered_apps(token, mgr_app_url)
+        if args.delete_apps:
+            print(f"Delete {app_id}")
+            delete_applications(token, app_id, mgr_app_url)
+            registered_apps(token, mgr_app_url)
+        if args.entitle:
+            print(f"Entitling {app_id}")
+            tenant_uuid = tenant_id(token, mgr_tenants_url)
+            entitle_applications(token, app_id, tenant_uuid, mgr_entitle_url)
+        if args.register_modules:
+            print(f"Registering modules for {app_id}")
+            for module in data["modules"]:
+                updated_module = module_discovery_object(module)
+                register_module(token, updated_module, mgr_app_url)
+        if args.reregister_modules:
+            print(f"Re-registering modules for {app_id}")
+            for module in data["modules"]:
+                updated_module = module_discovery_object(module)
+                re_register_module(token, updated_module, mgr_app_url)
+        if args.create_tenant:
+            print("Creating tenant")
+            tenant_uuid = create_tenant(token, mgr_tenants_url)
+            print(f"Created tenant sul, uuid: {tenant_uuid}")
+
+
+def register_applications(token, data, mgr_app_url):
+    with httpx.Client(timeout=20.0) as client:
+        try:
+            response = client.post(
+                f"{mgr_app_url}/applications", 
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}"
+                },
+                data=json.dumps(data)
+            )
+            print(response.status_code)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+
+
+def registered_apps(token, mgr_app_url):
+    with httpx.Client(timeout=20.0) as client:
+        try:
+            response = client.get(
+                f"{mgr_app_url}/applications?limit=50", 
+                headers={
+                    "Authorization": f"Bearer {token}"
+                }
+            )
+            print(response.status_code)
+            apps = json.loads(response.text).get("applicationDescriptors")
+            if len(apps) == 0:
+              print("No applications are registered")
+            else:
+                for i in apps:
+                    app_id = i["id"]
+                    print(f"{app_id} is registered")
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+
+
+def delete_applications(token, app_id, mgr_app_url):
+    with httpx.Client(timeout=20.0) as client:
+        try:
+            response = client.delete(
+                f"{mgr_app_url}/applications/{app_id}", 
+                headers={
+                    "Authorization": f"Bearer {token}"
+                }
+            )
+            print(response.status_code)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+
+
+def module_discovery_object(module) -> dict:
+    module_location = f"http://{module['name']}:8082"
+    module["location"] = module_location
+    module.pop("url", None) #removes url key from module dict
+    return module
+
+
+def register_module(token, module, mgr_app_url):
+    print(f"POSTING to /modules/{module['name']}/discovery")
+    print(module)
+    with httpx.Client(timeout=60.0) as client:
+        try:
+            response = client.post(
+                f"{mgr_app_url}/modules/{module['name']}/discovery",
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}"
+                },
+                data=json.dumps(module)
+            )
+            print(response.status_code)
+            response.raise_for_status()
+        except httpx.TimeoutException:
+            print("Request timed out!")
+            print("Retrying with a PUT")
+            re_register_module(token, module, mgr_app_url)
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+
+
+def re_register_module(token, module, mgr_app_url):
+    print(f"PUTTING to /modules/{module['name']}/discovery")
+    print(module)
+    with httpx.Client(timeout=60.0) as client:
+        try:
+            response = client.put(
+                f"{mgr_app_url}/modules/{module['name']}/discovery",
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}"
+                },
+                data=json.dumps(module)
+            )
+            print(response.status_code)
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            if exc.response.status_code == 401:
+                token = _token()
+                re_register_module(token, module, mgr_app_url)
+
+
+def create_tenant(token, mgr_tenants_url) -> str:
+    data = {
+        "name": "sul",
+        "description": "Stanford University Libraries"
+    }
+    with httpx.Client(timeout=20.0) as client:
+        try:
+            response = client.post(
+                f"{mgr_tenants_url}/tenants",
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}"
+                },
+                data=json.dumps(data)
+            )
+            print(response.status_code)
+            tenant_uuid = json.loads(response.text).get("id")
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+    return tenant_uuid
+
+
+def tenant_id(token, mgr_tenants_url) -> str:
+    with httpx.Client() as client:
+        try:
+            response = client.get(
+                f"{mgr_tenants_url}/tenants?query=name==sul",
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}"
+                }
+            )
+            print(response.status_code)
+            tenant_uuid = json.loads(response.text).get("id")
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+    return tenant_uuid
+
+
+def entitle_applications(token, app_id, tenant_uuid, mgr_entitle_url):
+    data = {
+        "tenantId": tenant_uuid,
+        "applications": [app_id]
+    }
+    with httpx.Client(timeout=60.0) as client:
+        try:
+            response = client.post(
+                f"{mgr_entitle_url}/entitlements?async=true&tenantParameters=loadReference=true,loadSample=false",
+                headers={
+                    "content-type": "application/json",
+                    "Authorization": f"Bearer {token}",
+                    "x-okapi-token": token
+                },
+                data=json.dumps(data)
+            )
+            print(response.status_code)
+            flow_id = json.loads(response.text).get("flowId")
+            print(f"Flow ID: {flow_id}")
+            response.raise_for_status()
+        except httpx.HTTPStatusError as exc:
+            print(exc)
+
+
+def _token():
+    kc_url = os.getenv("KC_URL", "http://keycloak:8080")
+    kc_admin_client_id = os.getenv("KC_ADMIN_CLIENT_ID", "folio-backend-admin-client")
+    kc_admin_client_secret = os.getenv("KC_ADMIN_CLIENT_SECRET")
+    print(f"fetching new token from {kc_url}")
+    response = httpx.post(f"{kc_url}/realms/master/protocol/openid-connect/token",
+        data={
+            "client_id": kc_admin_client_id,
+            "grant_type": "client_credentials",
+            "client_secret": kc_admin_client_secret,
+        }
+    )
+
+    print(response.status_code)
+    token = response.json()["access_token"]
+    return token
+
+
+if __name__ == "__main__":
+    if args.filename:
+        for file in args.filename:
+            main(file)
+    else:
+        descriptor_files = Path(args.namespace).glob("*.json")
+        for file in descriptor_files:
+            main(file)
